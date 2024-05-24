2165031f35672cae383328b486168689dd612405
Marin <51886896+mar1@users.noreply.github.com>
2024-05-24 14:55:13 +0200

    Adding Text/HSL component

---
+++ components/ColorSquare.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div class="square" :style="{ backgroundColor: `hsl(166, ${saturation}%, ${light}%)` }">
+      <p class="center-text">{{ text }}</p>
+    </div>
+    <div class="controls">
+      <label for="textInput">Text:</label>
+      <input id="textInput" type="text" v-model="text" />
+
+      <label for="saturationRange">Saturation:</label>
+      <input id="saturationRange" type="range" v-model="saturation" min="0" max="100" />
+
+      <label for="lightRange">Light:</label>
+      <input id="lightRange" type="range" v-model="light" min="0" max="100" />
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+      saturation: 100,
+      light: 50,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.square {
+  width: 500px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.center-text {
+  margin: 0;
+}
+
+.controls {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.controls label {
+  margin-top: 10px;
+}
+
+.controls input[type="range"] {
+  width: 300px;
+  margin-top: 5px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <ColorSquare />
 </template>
 
 <script>
